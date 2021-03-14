4bcccd29339d3b24c1e253e194fe15214ea091ea
Dhara-V-Patel <46474793+Dhara-V-Patel@users.noreply.github.com>
2021-03-14 10:45:44 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,42 +1,81 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import * as Location from 'expo-location'
+import * as Location from 'expo-location';
+import {WeatherInfo} from './components/WeatherInfo';
+import {UnitsPicker} from './components/UnitsPicker'
+
+
+
+const WEATHER_API_KEY = '97c145b30812ec20c4a13c505d68dd1e'
+const BASE_WEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather?'
 
 export default function App() {
 
-  const [errorMessage, setErrorMessage ] = useState(null)
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [currentWeather, setCurrentWeather] = useState(null)
+  const [unitSystem, setUnitSystem] = useState('metric')
 
   useEffect(() =>{
     load()
   }, [])
-  async function load() {
-    try{
-      let { } = await Location.requestPermissionsAsync()
+
+  async function load(){
+    try {
+      let { status } = await Location.requestPermissionsAsync()
       if(status != 'granted'){
-        setErrorMessage('Acess to location is needed to run the app')
+        setErrorMessage('Access to location is needed to run the app')
         return 
       }
       const location = await Location.getCurrentPositionAsync()
+      const {latitude, longitude} = location.coords
+      const weatherURL = `${BASE_WEATHER_URL}lat=${latitude}&lon=${longitude}&units=${unitSystem}&appid=${WEATHER_API_KEY}`
+      const response = await fetch(weatherURL)
+      const result = await response.json()
 
-      const { latitude, longitude } = location.coords
-      alert(`Latitude : ${latitude}, Longitude : ${longitude}`)
+      if(response.ok){
+        setCurrentWeather(result)
+      }else{
+        setErrorMessage(result.message)
+      }
+      
+      //alert(`Latitude : ${latitude}, Longitude : ${longitude}`)
 
-    }catch(error) {}
+    } catch (error) {
+      setErrorMessage(error.message)
+    }
   }
-  return (
-    <View style={styles.container}>
-      <Text>Hello from dhara</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  if(currentWeather){
+    
+    
+    return (
+      <View style={styles.container}>
+        <StatusBar style="auto" />
+        <View style={styles.main}>
+          <UnitsPicker />
+          <WeatherInfo currentWeather={currentWeather} />
+        </View>
+      </View>
+    );
+  }else{
+    return(
+      <View style={styles.container}>
+        <Text>{errorMessage}</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems : 'center',
   },
+  main: {
+    justifyContent : 'center',
+    alignItems: 'center',
+    alignSelf: 'center'
+  }
 });
